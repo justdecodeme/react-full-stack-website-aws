e2e8f1c69e691c60171ac21f9280565b03078e6e
Rakesh <justdecodeme@hotmail.com>
2021-01-02 16:50:31 +0530

    mongodb + express working and made first successful get api call

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
 
 const articlesInfo = {
 	"learn-react": {
@@ -22,6 +23,28 @@ app.use(bodyParser.json());
 // app.get("/hello", (req, res) => res.send("Hello!"));
 // app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
 app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
+
+app.get("/api/articles/:name", async (req, res) => {
+	try {
+		const articleName = req.params.name;
+
+		const client = await MongoClient.connect("mongodb://localhost:27017", {
+			useNewUrlParser: true,
+		});
+
+		const db = client.db("my-blog");
+
+		const articleInfo = await db
+			.collection("articles")
+			.findOne({ name: articleName });
+
+		res.status(200).json(articleInfo);
+
+		client.close();
+	} catch (error) {
+		res.status(500).json({ message: "Error connecting to db", error });
+	}
+});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
 	const articleName = req.params.name;
