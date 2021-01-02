ecb0655d145416dc2ff725b921610f2583fa8354
Rakesh <justdecodeme@hotmail.com>
2021-01-02 18:16:28 +0530

    first api call to front end

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
@@ -8,16 +8,28 @@ export default function ArticlePage({ match }) {
 	const name = match.params.name;
 	const article = articleContent.find((article) => article.name === name);
 
+	// setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetch(`api/articles/${name}`);
+			const body = await result.json();
+			setArticleInfo(body);
+		};
+
+		fetchData();
+	}, [name]);
+
 	if (!article) return <NoFoundPage />;
 	// if (!article) return <h1>Article doesn't exist!</h1>;
 
-	const otehrArticles = articleContent.filter(
-		(article) => article.name !== name
-	);
+	const otehrArticles = articleContent.filter((article) => article.name !== name);
 
 	return (
 		<>
 			<h1>{article.title}</h1>
+			<p>This post has been upvoted {articleInfo.upvotes} times</p>
 			{article.content.map((paragraph, key) => (
 				<p key={key}>{paragraph}</p>
 			))}
